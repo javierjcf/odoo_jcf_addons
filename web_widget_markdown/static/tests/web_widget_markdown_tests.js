437d17fa141d82de2682924da30d6bfeca8de484
javierjcf <javier@comunitea.com>
2023-02-08 20:59:08 +0100

    [ADD]basic Test

--- web_widget_markdown/static/tests/web_widget_markdown_tests.js
+++ web_widget_markdown/static/tests/web_widget_markdown_tests.js
@@ -3,10 +3,60 @@ odoo.define('web_widget_markdown_tests', function (require) {
     var FormView = require('web.FormView');
     var testUtils = require('web.test_utils');
 
-    QUnit.module('Markdown Widget Tests', {}, function () {
-        QUnit.only('Test something', async function(assert) {
-            assert.expect(1); // number of assertion we have in this
-            assert.strictEqual(1, true);
-        })
-    })
+    // argumentos:
+    // 1 - Nombre test
+    // 2 - Darta que se le pueden pasar a todos nuestros test
+    // 3 - Función que contiene los test individuales
+    QUnit.module('Markdown Widget Tests', {
+        beforeEach: function () {
+            this.data = {
+                blog: { 
+                    fields: {
+                        name: {
+                            string: "Name", 
+                            type: "char"
+                        },
+                        content: { 
+                            string: "Content", 
+                            type: "text"
+                        },
+                    },
+                    records: [
+                        {
+                            id: 1, name: "Blog Post 1", 
+                            content: "# Hello world",
+                        }
+                    ]
+                }
+            };
+        }}, 
+        function () {
+            QUnit.only('web_widget_markdown test suite', async function(assert) {
+                assert.expect(2);
+                var form = await testUtils.createView({
+                    View: FormView,
+                    model: 'blog',
+                    data: this.data,
+                    arch: '<form string="Blog">' +
+                            '<group>' +
+                                '<field name="name"/>' +
+                                '<field name="content" widget="markdown"/>' +
+                            '</group>' +
+                        '</form>',
+                    res_id: 1,
+                });
+                assert.strictEqual(
+                    form.$('.o_field_markdown').find("h1").length, 
+                    1, 
+                    "h1 should be present"
+                );
+                assert.strictEqual(
+                    form.$('.o_field_markdown h1').text(), 
+                    "Hello world", 
+                    "<h1> should contain 'Hello world'"
+                );
+                form.destroy();
+            });
+        }
+    );
  })
