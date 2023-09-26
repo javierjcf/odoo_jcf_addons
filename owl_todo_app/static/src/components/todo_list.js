2f88f04dada70ad6bff5e4868d4e8eb230398f67
javierjcf <javier@comunitea.com>
2023-09-26 18:03:49 +0200

    [ADD]owl_todo_app: Sacar las tareas del ORM en lugar de tenerlas estáticas

--- owl_todo_app/static/src/components/todo_list.js
+++ owl_todo_app/static/src/components/todo_list.js
@@ -1,17 +1,29 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
-const { Component, useState } = owl;
+const { Component, useState, onWillStart } = owl;
+import { useService } from "@web/core/utils/hooks";
+
 
 export class OwlTodoList extends Component {
     setup(){
         this.state = useState({
             value: 1,
             taskList: [
-                {id:1, name:"Task 1", color:"#FF0000", completed:true},
-                {id:2, name:"Task 2", color:"#000000", completed:false},
-                {id:3, name:"Task 3", color:"#FFFFFF", completed:true},
+                // {id:1, name:"Task 1", color:"#FF0000", completed:true},
+                // {id:2, name:"Task 2", color:"#000000", completed:false},
+                // {id:3, name:"Task 3", color:"#FFFFFF", completed:true},
             ]
         });
+        this.orm = useService("orm");
+        this.model = "owl.todo.list"
+
+        onWillStart(async () => {
+            await this.getAllTask();
+        })
+    }
+
+    async getAllTask(){
+        this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "color", "completed"]);
     }
 }
 
