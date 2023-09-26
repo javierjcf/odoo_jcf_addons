39c9af97a0718ce44c8a07f5af49218390fa6fa8
javierjcf <javier@comunitea.com>
2023-09-26 17:33:14 +0200

    [ADD]Tareas creadas esde setup function

--- owl_todo_app/static/src/components/todo_list.js
+++ owl_todo_app/static/src/components/todo_list.js
@@ -6,6 +6,11 @@ export class OwlTodoList extends Component {
     setup(){
         this.state = useState({
             value: 1,
+            taskList: [
+                {id:1, name:"Task 1", color:"#FF0000", completed:true},
+                {id:2, name:"Task 2", color:"#000000", completed:false},
+                {id:3, name:"Task 3", color:"#FFFFFF", completed:true},
+            ]
         });
     }
 }
