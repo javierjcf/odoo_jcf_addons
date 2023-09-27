8cc2a85951baaf1bbd0a69ae9c097429e0e82d79
javierjcf <javier@comunitea.com>
2023-09-27 10:24:45 +0200

    [ADD]Búsqueda de tareas, y eventos vista lista

--- owl_todo_app/static/src/components/todo_list.js
+++ owl_todo_app/static/src/components/todo_list.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
-const { Component, useState, onWillStart } = owl;
+const { Component, useState, onWillStart, useRef } = owl;
 import { useService } from "@web/core/utils/hooks";
 
 
@@ -15,6 +15,7 @@ export class OwlTodoList extends Component {
         });
         this.orm = useService("orm");
         this.model = "owl.todo.list"
+        this.searchInput = useRef("search-input");
 
         onWillStart(async () => {
             await this.getAllTask();
@@ -59,6 +60,19 @@ export class OwlTodoList extends Component {
         // Reinicio el formulario del modal para que no recuerde los datos
         // por ejemplo si editamos antes
         this.state.task = {name: "", color: "#FF0000", completed: false};
+    }
+
+    async searchTasks(){
+        const text = this.searchInput.el.value;
+        this.state.taskList = await this.orm.searchRead(this.model, [['name', 'ilike', text]], ["name", "color", "completed"]);
+    }
+    async toggleTask(e, task){
+        await this.orm.write(this.model, [task.id], {completed: e.target.checked});
+        await this.getAllTask();
+    }
+    async updateColor(e, task){
+        await this.orm.write(this.model, [task.id], {color: e.target.value});
+        await this.getAllTask();
     }
 }
 
